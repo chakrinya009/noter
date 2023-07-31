b509ca93a98d536fdf9b3be62a24faf07f3ed89c
chakrinya <chakrinyabalabadra@gmail.com>
2023-07-31 15:05:16 +0530

    first commit

---
+++ server/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+const connectDB = async () => {
+  await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  try {
+    console.log("database connected");
+  } catch (err) {
+    console.log(err);
+  }
+};
+module.exports = connectDB;
